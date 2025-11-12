0ca56d26e2b47c66b7a8e9e69dd1e5f47eb80311
Jaiime07 <jaimarpal@alum.us.es>
2025-11-12 22:44:40 +0100

    Ejercicios T4 empezados

---
+++ src/cadenas.py
@@ -0,0 +1,64 @@
+def invierte_cadena(texto) -> str:
+    inv = ''
+    for c in texto:
+        inv = c + inv
+    return inv
+
+def es_palindromo(texto, ignora_espacios, ignora_mayusculas):
+    
+    if ignora_espacios:
+        texto = texto.replace(" ", "")
+
+    if ignora_mayusculas:
+        texto = texto.lower()
+
+    palindromo = False
+    if texto == invierte_cadena(texto):
+        palindromo = True
+    return palindromo
+    
+def estiliza_mensaje(texto, alterna_may_min = True, usa_dieresis = False, sustituye_espacios = " ") -> str | bool | bool | str:
+    if alterna_may_min:
+        contador = 0
+        alternado = ""
+        for c in texto:
+            if c.isalpha():
+                contador += 1
+                if contador % 2 != 0:
+                    alternado = alternado + c.upper() 
+                elif contador % 2 == 0:
+                    alternado = alternado + c.lower() 
+            else:
+                alternado = alternado + c
+            
+        return alternado
+ 
+    if usa_dieresis:
+        texto_dieresis = ""
+        for c in texto:
+        
+            if c.lower() == "a":
+                texto_dieresis = texto_dieresis+"ä"
+            elif c.lower() == "e":
+                texto_dieresis = texto_dieresis+"é"
+            elif c.lower() == "i":
+                texto_dieresis = texto_dieresis+"ï"
+            elif c.lower() == "o":
+                texto_dieresis = texto_dieresis+"ö"
+            elif c.lower() == "u":
+                texto_dieresis = texto_dieresis+"ü"
+            else:
+                texto_dieresis = texto_dieresis + c
+        return texto_dieresis
+    
+    if sustituye_espacios != "":
+        texto_espacios = ""
+        for c in texto:
+            if c == " ":
+                h = texto.replace(c, sustituye_espacios)
+                texto_espacios = texto_espacios + h
+        return texto
+    #está mal, no se puede usar un return en cada uno porque queremos poder aplicar las 3 funciones al mismo texto 
+    
+
+print(estiliza_mensaje("Murciélago", usa_dieresis=True))
--- src/cadenas_test.py
+++ src/cadenas_test.py
@@ -1,5 +1,6 @@
+
 from cadenas import estiliza_mensaje
-  
+
 def test_estiliza_mensaje():
     print("Probando estiliza_mensaje...")
     assert estiliza_mensaje("Fundamentos de programación 1") == "FuNdAmEnToS dE pRoGrAmAcIóN 1"
@@ -9,4 +10,20 @@ def test_estiliza_mensaje():
     assert estiliza_mensaje("Hola Mundo", alterna_may_min=True, usa_dieresis=False, sustituye_espacios="_") == "HoLa_MuNdO"
 
 test_estiliza_mensaje()
+
+
+from cadenas import invierte_cadena, es_palindromo
+
+def test_invierte_cadena():
+    assert invierte_cadena("Texto de prueba") == "abeurp ed otxeT"
+    assert invierte_cadena("seres") == "seres"
+
+def test_es_palindromo():
+    assert es_palindromo("reconocer", False, False) == True
+    assert es_palindromo("antonio", False, False) == False
+    assert es_palindromo("Amor a Roma", False, True) == True
+    assert es_palindromo("Luz azul", True, True) == True
+
+test_invierte_cadena()
+test_es_palindromo()
 print("Todas las pruebas pasaron correctamente.")
